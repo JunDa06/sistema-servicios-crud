f44e178e9dd98b63ea0d6122f6dddf9701580518
JunDa <dayroncavero0603@gmail.com>
2026-04-20 11:49:06 -0500

    fix: recrear app.js correctamente en carpeta server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+
+let servicios = [];
+
+app.get("/servicios", (req, res) => {
+  res.json(servicios);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
