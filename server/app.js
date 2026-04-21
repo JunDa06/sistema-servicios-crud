68cf8dd97b4d8609c1d0ad972a9ef0a6d072ad0b
JunDa <dayroncavero0603@gmail.com>
2026-04-20 20:43:46 -0500

    feat: conectar base de datos en la nube con Railway

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const sequelize = require("./database");
+const bcrypt = require("bcryptjs");
 
 const app = express();
+
+// 🔹 MODELO USER
+const User = require("./models/user.model");
 
 // 🔹 Middlewares
 app.use(cors());
@@ -28,10 +32,30 @@ app.use((req, res) => {
 // 🔹 Puerto
 const PORT = 3000;
 
-// 🔥 Conectar BD y levantar servidor
+// 🔥 Conectar BD + CREAR USUARIO ADMIN
 sequelize.sync()
-  .then(() => {
+  .then(async () => {
     console.log("Base de datos conectada 🟢");
+
+    try {
+      const existe = await User.findOne({ where: { username: "admin" } });
+
+      if (!existe) {
+        const hashedPassword = await bcrypt.hash("1234", 10);
+
+        await User.create({
+          username: "admin",
+          password: hashedPassword
+        });
+
+        console.log("Usuario admin creado 🔐");
+      } else {
+        console.log("El usuario admin ya existe");
+      }
+
+    } catch (error) {
+      console.error("Error con User:", error);
+    }
 
     app.listen(PORT, () => {
       console.log(`Servidor corriendo en http://localhost:${PORT}`);
--- server/database.js
+++ server/database.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite"
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,       // railway
+  process.env.DB_USER,       // root
+  process.env.DB_PASSWORD,   // password
+  {
+    host: process.env.DB_HOST, // mysql.railway.internal
+    port: process.env.DB_PORT, // 3306
+    dialect: "mysql",
+    logging: false
+  }
+);
 
 module.exports = sequelize;
